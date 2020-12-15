a9ddb2d1126fc72433b764967d6589a54ee3261f
kevinmenden <kevin.menden@t-online.de>
2020-12-15 15:59:55 +0100

    switched to pattern in schema option

--- lib/Validation.groovy
+++ lib/Validation.groovy
@@ -16,12 +16,16 @@ class Validation {
                         'bg', 'h', 'log', 'quiet', 'q', 'v', 'version']
         def valid_params = []
         def expected_params = []
+        def blacklist  = ['hostnames'] // ignored parameters
+        
 
         // Loop over all parameters in schema and compare to given parameters
         for (group in json_params){
             for (p in group.value['properties']){
-                valid_params.push(validateParamPair(params[p.key], p, log))
-                expected_params.push(p.key)
+                if (!blacklist.contains(p.key)){
+                    valid_params.push(validateParamPair(params[p.key], p, log))
+                    expected_params.push(p.key)
+                }
             }
         }
 
@@ -40,6 +44,8 @@ class Validation {
 
     }
 
+
+
     /*
     * Compare a pair of params (schema, command line) and check whether 
     * they are valid
@@ -49,9 +55,7 @@ class Validation {
         def valid_param = false
         def required = json_param.value['required']
         def param_enum = json_param.value['enum']
-        // Get the expected class
-        def schema_param_class = determineDefaultClass(json_param)
-
+        def param_pattern = json_param.value['pattern']
         
         // Check only if required or parameter is given
         if (required || given_param){
@@ -59,26 +63,18 @@ class Validation {
 
                 switch(param_type) {
                     case 'string':
-                        // memory
-                        if (schema_param_class == nextflow.util.MemoryUnit || given_param_class == nextflow.util.MemoryUnit){
-                            valid_param = given_param.toString() ==~ /\d+\.?\s*[KMGT]?B?/
-                        }
-                         // duration
-                        else if (schema_param_class == nextflow.util.Duration || given_param_class == nextflow.util.Duration){
-                            valid_param = given_param.toString() ==~ /\d+\.?\s*[mhd]/
-                        }
-                         // hashmap
-                        else if (schema_param_class == LinkedHashMap || given_param_class == LinkedHashMap){
-                            valid_param = true // TODO change this later too
-                        }
-                         // enum
+                        // If pattern given, check that param adheres to it
+                        if (param_pattern){
+                            valid_param = given_param ==~ param_pattern
+                        } 
+                        // If enum given, check that param is within choices
                         else if (param_enum){
                             valid_param = param_enum.contains(given_param)
                         }
-                        // normal string
+                        // else just check whether valid String
                         else {
                             valid_param = given_param_class == String
-                        }  
+                        } 
                         break
                     case 'boolean':
                         if (given_param_class == Boolean){
@@ -97,9 +93,12 @@ class Validation {
                 }
 
             if (!valid_param){
-                log.error "ERROR: Parameter ${json_param.key} is wrong type! Expected ${schema_param_class}, found ${param_type}, ${given_param}"
+                log.error "ERROR: Parameter ${json_param.key} is wrong type! Expected ${param_type}, found ${given_param_class}, ${given_param}"
                 if (param_enum){
                     log.error "Must be one of: ${param_enum}"
+                }
+                if (param_pattern){
+                    log.error "Parameter must adhere to the following pattern: ${param_pattern}"
                 }
             }
 
@@ -107,26 +106,4 @@ class Validation {
         return valid_param
      }
 
-    private static Class determineDefaultClass(json_param){
-        def default_value = json_param.value['default']
-        def schema_param_class = default_value.getClass()
-
-        // If value is not null, try to cast to other objects
-        if (default_value){
-            // try memory object
-            try {
-                default_value as nextflow.util.MemoryUnit
-                schema_param_class = nextflow.util.MemoryUnit
-            }catch (Exception) {}
-
-            // try duration object
-            try {
-                default_value as nextflow.util.Duration
-                schema_param_class = nextflow.util.Duration
-            } catch (Exception) {}
-        }
-
-
-        return schema_param_class
-    }
 }
