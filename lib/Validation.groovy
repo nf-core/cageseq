a3aa6e9a489a928f6325ef32eb390795b8344163
kevinmenden <kevin.menden@t-online.de>
2020-12-30 12:49:17 +0100

    added option to skip; returning unexpected params to wf

--- lib/Validation.groovy
+++ lib/Validation.groovy
@@ -16,7 +16,7 @@ class Validation {
     * whether the given paremeters adhere to the specificiations
     */
     /* groovylint-disable-next-line UnusedPrivateMethodParameter */
-    private static void validateParameters(params, jsonSchema, log) {
+    private static ArrayList validateParameters(params, jsonSchema, log) {
         //=====================================================================//
         // Validate parameters against the schema
         InputStream inputStream = new File(jsonSchema).newInputStream()
@@ -37,6 +37,7 @@ class Validation {
             log.error 'Found parameter violations!'
             JSONObject exceptionJSON = e.toJSON()
             printExceptions(exceptionJSON, log)
+            System.exit(1)
         }
 
         // Check for nextflow core params and unexpected params
@@ -45,6 +46,7 @@ class Validation {
         def specified_param_keys = params.keySet()
         def nf_params = ['profile', 'config', 'c', 'C', 'syslog', 'd', 'dockerize',
                         'bg', 'h', 'log', 'quiet', 'q', 'v', 'version']
+        def unexpectedParams = []
 
         // Collect expected parameters from the schema
         def expectedParams = []
@@ -58,13 +60,15 @@ class Validation {
             // nextflow params
             if (nf_params.contains(specified_param)) {
                 log.error "ERROR: You used a core Nextflow option with two hyphens: --${specified_param}! Please resubmit with one."
-                System.exit(0)
+                System.exit(1)
             }
             // unexpected params
             if (!expectedParams.contains(specified_param)) {
-                log.warn "Unexpected parameter specified: ${specified_param}"
+                unexpectedParams.push(specified_param)
             }
         }
+
+        return unexpectedParams
     }
 
     // Loop over nested exceptions and print the causingException
@@ -88,13 +92,15 @@ class Validation {
             if (!p['value']) {
                 new_params.remove(p.key)
             }
-
+            // Cast MemoryUnit to String
             if (p['value'].getClass() == nextflow.util.MemoryUnit) {
                 new_params.replace(p.key, p['value'].toString())
             }
+            // Cast Duration to String
             if (p['value'].getClass() == nextflow.util.Duration) {
                 new_params.replace(p.key, p['value'].toString())
             }
+            // Cast LinkedHashMap to String
             if (p['value'].getClass() == LinkedHashMap) {
                 new_params.replace(p.key, p['value'].toString())
             }
